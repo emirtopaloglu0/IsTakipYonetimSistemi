d2d2e8162ef8b0053a459ca8bd4f211d898cc61b
emirtopaloglu0 <emirt2048@gmail.com>
2024-06-14 18:04:32 +0300

    Refactoring

--- IsTakipYonetimSistemi/View/IlerlemeEkleForm.cs
+++ IsTakipYonetimSistemi/View/IlerlemeEkleForm.cs
@@ -25,6 +25,28 @@ namespace IsTakipYonetimSistemi.View
             instance = this;
         }
 
+        internal void LoadData()
+        {
+            Progress_Listbox.Items.Clear();
+
+            var progresses = GetProgressList();
+            var project = GetProject();
+
+            Progress_Listbox.Items.Add($"Proje: {project.Ad}");
+            Progress_Listbox.Items.Add($"Açıklama: {project.Aciklama}");
+
+            CheckProgressLevel(project);
+
+            if (IsProgressCountLowerZero(progresses))
+                return;
+
+            foreach (var progress in progresses)
+            {
+                Progress_Listbox.Items.
+                    Add($"İlerleme: %{progress.Ilerleme_Yuzdesi} - Açıklama: {progress.Aciklama}");
+                progressBar1.Value = progress.Ilerleme_Yuzdesi;
+            }
+        }
         internal Proejeler GetProject()
         {
             return DB_Connection.db.Proejeler.Find(proje_Id);
@@ -36,115 +58,142 @@ namespace IsTakipYonetimSistemi.View
             return DB_Connection.db.Ilerlemeler.Where(x => x.Proje_Id == proje.Id).ToList();
         }
 
-
-
-        internal void LoadData()
+        private void CheckProgressLevel(Proejeler project)
         {
-            Progress_Listbox.Items.Clear();
-
-            var progresses = GetProgressList();
-            var project = GetProject();
-
             if ((project.Ilerleme == 100))
             {
                 Ilerleme_Textbox.Enabled = false;
                 IlerlemeAciklama_Richbox.Enabled = false;
                 AddProgress_Btn.Enabled = false;
             }
+        }
 
-            Progress_Listbox.Items.Add($"Proje: {project.Ad}");
-            Progress_Listbox.Items.Add($"Açıklama: {project.Aciklama}");
-
+        private bool IsProgressCountLowerZero(List<Ilerlemeler> progresses)
+        {
             if (progresses.Count <= 0)
             {
                 Progress_Listbox.Items.Add("Şu Anda Projede Hiçbir İlerleme Bulunmamaktadır.");
                 progressBar1.Value = 0;
-                return;
+                return true;
             }
-
-            foreach (var progress in progresses)
-            {
-                Progress_Listbox.Items.
-                    Add($"İlerleme: %{progress.Ilerleme_Yuzdesi} - Açıklama: {progress.Aciklama}");
-                progressBar1.Value = progress.Ilerleme_Yuzdesi;
-            }
+            return false;
         }
 
-        private void Close_Btn_Click(object sender, EventArgs e)
-        {
 
-            ShowProjects.instance.LoadData(CallProjects.OngoingProjects());
-            Close();
-        }
+       
 
         private void AddProgress_Btn_Click(object sender, EventArgs e)
         {
             try
             {
-                if (IlerlemeAciklama_Richbox.Text != string.Empty && Ilerleme_Textbox.Text != string.Empty)
+                SoruMesajlari.instance.IlerlemeKayit();
+                if (SoruMesajlari.instance.res == DialogResult.Yes)
                 {
-                    if (Int16.Parse(Ilerleme_Textbox.Text) == 100)
-                    {
-                        SoruMesajlari.instance.ProjeBitiyor();
-                        if (SoruMesajlari.instance.res == DialogResult.No)
-                        {
-                            return;
-                        }
-                        else
-                            goto devam;
-                    }
+                    var aciklama = IlerlemeAciklama_Richbox.Text.Trim();
+                    var ilerlemeYuzdesi = Ilerleme_Textbox.Text.Trim();
+                    var convertedIlerlemeYuzdesi = Int16.Parse(ilerlemeYuzdesi);
 
 
-                    SoruMesajlari.instance.IlerlemeKayit();
-                devam:
-                    if (SoruMesajlari.instance.res == DialogResult.Yes)
-                    {
+                    if (!IsProgressLevelCorrect(convertedIlerlemeYuzdesi))
+                        return;
 
-                        var progress = DB_Connection.db.Ilerlemeler.Where(x => x.Proje_Id == proje_Id).ToList();
+                    if (IsEmpty(aciklama, ilerlemeYuzdesi))
+                        return;
 
-                        foreach (var item in progress)
-                        {
-                            if (Int32.Parse(Ilerleme_Textbox.Text) <= item.Ilerleme_Yuzdesi || Int32.Parse(Ilerleme_Textbox.Text) < 0 || Int32.Parse(Ilerleme_Textbox.Text) > 100)
-                            {
-                                HataMesajlari.IlerlemeYanlis();
-                                return;
-                            }
+                    if (!IsProjectOver(convertedIlerlemeYuzdesi))
+                        return;
 
-                        }
+                    var progresses = DB_Connection.db.Ilerlemeler.Where(x => x.Proje_Id == proje_Id).ToList();
 
+                    if (!IsNewProgressLevelValid(progresses, convertedIlerlemeYuzdesi))
+                        return;
 
+                    AddProgressToDb(proje_Id, convertedIlerlemeYuzdesi, aciklama);
 
+                    IlerlemeAciklama_Richbox.Text = string.Empty;
+                    Ilerleme_Textbox.Text = string.Empty;
 
-                        Ilerlemeler ilerleme = new Ilerlemeler();
-                        ilerleme.Proje_Id = proje_Id;
-                        ilerleme.Ilerleme_Yuzdesi = Int16.Parse(Ilerleme_Textbox.Text);
-                        ilerleme.Aciklama = IlerlemeAciklama_Richbox.Text.Trim();
-                        DB_Connection.db.Ilerlemeler.Add(ilerleme);
-
-                        var proje = DB_Connection.db.Proejeler.Find(proje_Id);
-
-                        proje.Ilerleme = Int16.Parse(Ilerleme_Textbox.Text);
-
-                        if (Int16.Parse(Ilerleme_Textbox.Text) == 100)
-                        {
-                            proje.IsDone = true;
-                            BasariliMesajlar.ProjeBitti();
-                        }
-                        DB_Connection.db.SaveChanges();
-                        IlerlemeAciklama_Richbox.Text = string.Empty;
-                        Ilerleme_Textbox.Text = string.Empty;
-                        LoadData();
-                    }
-
+                    LoadData();
                 }
-                else
-                    HataMesajlari.KontrolEdiniz();
             }
             catch (Exception ex)
             {
                 HataMesajlari.CatchError(ex);
             }
 
+        }
+
+        private void AddProgressToDb(int proje_Id, short convertedIlerlemeYuzdesi, string aciklama)
+        {
+            Ilerlemeler ilerleme = new Ilerlemeler();
+            ilerleme.Proje_Id = proje_Id;
+            ilerleme.Ilerleme_Yuzdesi = convertedIlerlemeYuzdesi;
+            ilerleme.Aciklama = aciklama;
+            DB_Connection.db.Ilerlemeler.Add(ilerleme);
+
+            var proje = DB_Connection.db.Proejeler.Find(proje_Id);
+            proje.Ilerleme = convertedIlerlemeYuzdesi;
+
+            if (convertedIlerlemeYuzdesi == 100)
+            {
+                proje.IsDone = true;
+                BasariliMesajlar.ProjeBitti();
+            }
+
+            DB_Connection.db.SaveChanges();
+        }
+
+        private bool IsNewProgressLevelValid(List<Ilerlemeler> progresses, short convertedIlerlemeYuzdesi)
+        {
+            foreach (var progress in progresses)
+            {
+                if (convertedIlerlemeYuzdesi <= progress.Ilerleme_Yuzdesi)
+                {
+                    HataMesajlari.IlerlemeYanlis();
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool IsProgressLevelCorrect(short convertedIlerlemeYuzdesi)
+        {
+            if (convertedIlerlemeYuzdesi < 0 || convertedIlerlemeYuzdesi > 100)
+            {
+                HataMesajlari.IlerlemeYanlis();
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsEmpty(string aciklama, string ilerlemeYuzdesi)
+        {
+            if (aciklama != string.Empty && ilerlemeYuzdesi != string.Empty)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool IsProjectOver(short convertedIlerlemeYuzdesi)
+        {
+            if (convertedIlerlemeYuzdesi == 100)
+            {
+                SoruMesajlari.instance.ProjeBitiyor();
+                if (SoruMesajlari.instance.res == DialogResult.No)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        private void Close_Btn_Click(object sender, EventArgs e)
+        {
+
+            ShowProjects.instance.LoadData(CallProjects.OngoingProjects());
+            Close();
         }
     }
 }
